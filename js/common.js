b03e222fc05d4983ab9a4f64e7d9f8efbb8dfe6b
poin.z <skskgus7516@gmail.com>
2024-08-22 17:13:41 +0900

    portfolio-site
    portfolio 01

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('#header').load('common/header.html');
+    $('#footer').load('common/footer.html');
+    $('#loding').load('common/loding.html');
+    $('#bar').load('common/rightBar.html')
+});
---
+++ js/index.js
@@ -0,0 +1,58 @@
+$(function () {
+    window.onload = function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    } //새로고침하면 스크롤 상단으로 이동
+
+    let tabMenu = $('.tab_menu li');
+    let tabList = $('.tab_list');
+
+    tabList.not('.on').hide();
+
+    tabMenu.click(function () {
+        let index = $(this).index();
+
+        tabMenu.removeClass('on');
+        tabList.removeClass('on');
+
+        $(this).addClass('on');
+        tabList.eq(index).addClass('on');
+        tabList.hide();
+        tabList.eq(index).show();
+    });
+
+    let webDesign = $('#webdesign');
+    let tabMenuFixed = $('.tab_menu');
+    let item = $('#graphicdesign');
+    let header = $('#header');
+
+    console.log(webDesign.offset().top);
+    console.log(webDesign.height());
+
+    let webDesignTop = webDesign.offset().top;
+    let itemTop = item.offset().top;
+
+    $(window).scroll(function () {
+        let scrollBar = $(window).scrollTop();
+        console.log(scrollBar);
+        if (scrollBar > webDesignTop + 50 && scrollBar < itemTop - 800) {
+            tabMenuFixed.addClass('active');
+            header.children('.header').addClass('active')
+        } else {
+            tabMenuFixed.removeClass('active')
+            header.children('.header').removeClass('active')
+        }
+    });
+
+    let scrollBtn = $('.scroll');
+
+    scrollBtn.click(function () {
+        let aboutConst = $('.about');
+        $('html, body').animate({
+            scrollTop: aboutConst.offset().top
+        }, 2000, 'swing');
+    });
+
+});
