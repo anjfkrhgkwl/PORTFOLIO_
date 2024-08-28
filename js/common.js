ddb49dbfc7cd879e185c7b2f59c608ffbb0212ba
poin.z <skskgus7516@gmail.com>
2024-08-28 11:06:06 +0900

    2024

--- js/common.js
+++ js/common.js
@@ -1,6 +1,5 @@
 $(function(){
     $('#header').load('common/header.html');
     $('#footer').load('common/footer.html');
-    $('#loding').load('common/loding.html');
     $('#bar').load('common/rightBar.html')
 });
